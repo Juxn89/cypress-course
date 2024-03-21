1fd108836f735f1af01134dff9dea17bbc18edfb
Juan Gómez <gomezj01@stepstone.com>
2024-03-21 09:55:12 -0600

    Primera prueba en Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    excludeSpecPattern: [
+      '**/1-getting-started/*.js',
+      '**/2-advanced-examples/*.js',
+    ],
+    viewportHeight: 1920,
+    viewportWidth: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,5 @@
+describe('First Test', () => {
+  it('Browse to first page', () => {
+    cy.visit('https://www.platzi.com')
+  });
+})
