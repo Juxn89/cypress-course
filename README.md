82983ca3c09f1e1ebc2119b5a1d85406376328dc
Juan Gómez <gomezj01@stepstone.com>
2024-04-05 08:36:21 -0600

    Trabajando con checkboxes y radio botones

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Cypress
 Curso de **Automatización de Pruebas UI** con ```Cypress``` de ```Platzi```
+
+## Comandos
+### Types
+Lista de algunos comandos para usar con el type:
+```
+	{backspace} Borra el personaje a la izquierda del cursor.
+	{del} Borra el personaje a la derecha del cursor.
+	{downarrow} Mueve el cursor hacia abajo.
+	{end}	Mueve el cursor al final de la línea.
+	{enter} Teclea la tecla Intro.
+	{esc}	Teclea la tecla Escape.
+	{home} Mueve el cursor al principio de la línea.
+	{insert} Inserta un personaje a la derecha del cursor.
+	{leftarrow} Mueve el cursor a la izquierda.
+	{movetoend} Desplaza el cursor al final del elemento mecanizable.
+	{movetostart} Desplaza el cursor al inicio del elemento mecanizable.
+	{pagedown} Se desplaza hacia abajo.
+	{pageup}  Se desplaza hacia arriba.
+	{rightarrow} Mueve el cursor a la derecha.
+	{selectall} Selecciona todo el texto creando un selection range.
+	{uparrow}	Mueve el cursor hacia arriba.
+```
--- cypress/e2e/07-eventListener.cy.js
+++ cypress/e2e/07-eventListener.cy.js
@@ -23,10 +23,34 @@ describe('Event listener in elements', () => {
 		cy.get('#enableAfter').click({ timeout: 0, force: true })
 	})
 
-	it.only('Click by possition', () => {
+	it('Click by possition', () => {
 		cy.visit('/buttons')
 		cy.get('button').eq(3).parent().parent().click('topRight')
 		cy.get('button').eq(3).parent().parent().click('bottomLeft')
 		cy.get('button').eq(3).parent().parent().click(5, 60)
+	})	
+	
+	it('Input type text', () => {
+		cy.visit('/automation-practice-form')
+		cy.get('#firstName').type('Juan')
+		cy.get('#lastName').type('Gomez')
+		
+		cy.get('#firstName').type('{selectAll}{backspace}')
+		cy.get('#firstName').type('Carlos')
+		cy.get('#firstName').clear()
+	})	
+	
+	it.only('Checkbox and radio buttons', () => {
+		cy.visit('/automation-practice-form')
+		// cy.get('#gender-radio-1').click({ force: true })
+		// cy.get('#gender-radio-1').check({ force: true })
+		cy.get('label[for="gender-radio-1"]').click()
+
+		// cy.get('#hobbies-checkbox-1').click({ force: true})
+		// cy.get('#hobbies-checkbox-1').check({ force: true})
+		// cy.get('#hobbies-checkbox-1').check({ force: true})
+		// cy.get('#hobbies-checkbox-1').uncheck({ force: true})
+
+		cy.get('label[for="hobbies-checkbox-1"]').click()
 	})
 })
