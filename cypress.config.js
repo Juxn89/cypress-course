814fc128c7cb8375af1d1dd1ee56b0f178bf255c
Juan Gómez <gomezj01@stepstone.com>
2024-04-09 11:42:58 -0600

    Clean up and set-up the project.

--- cypress.config.js
+++ cypress.config.js
@@ -2,22 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-		setupNodeEvents(on, config) {
-			on('task', {
-				log(message) { 
-					console.log(`I'm the console.log from Task: ${ message }`)
-					return null
-				}
-			})
-		},
-    baseUrl: 'https://demoqa.com',
-    excludeSpecPattern: [
-      '**/1-getting-started/*.js',
-      '**/2-advanced-examples/*.js',
-    ],
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-		testIsolation: false,
-		video: true
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
