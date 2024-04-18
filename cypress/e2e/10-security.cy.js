7e2136ae6a82d48ba3c306ba7635425af17c1490
Juan Gómez <gomezj01@stepstone.com>
2024-04-18 15:03:34 -0600

    Navigate between differents domains in differents test

--- cypress/e2e/10-security.cy.js
+++ cypress/e2e/10-security.cy.js
@@ -1,3 +1,5 @@
+let label;
+
 describe('Security', () => {
 	it('Browse between differents domains', () => {
 		cy.visit('/')
@@ -5,13 +7,34 @@ describe('Security', () => {
 		cy.get('#title').type('Test title')
 	})
 
-	it.only('Navigate to a domain', function() {
+	it('Navigate to a domain', function() {
 		cy.visit('https://todo-cypress-iota.vercel.app')
 		cy.get('h1').invoke('text').as('title')
 	})
 
-	it.only('Navigate to another domain', function() {
+	it('Navigate to another domain', function() {
 		cy.visit('https://todo-cypress-iota.vercel.app')
 		cy.log(this.title)
+	})
+
+	it.only('Navigate between 2 domains in the same test', () => {
+		cy.visit('/')
+		cy.get('h1').first().invoke('text').then((text) => {
+			label = text
+			Cypress.env({ envLabel: text })
+		})
+
+		cy.origin(
+			'https://todo-cypress-iota.vercel.app', 
+			{ args: { label } }, 
+			function({ label }) {
+				cy.visit('/')
+				cy.log(label)
+				cy.log(Cypress.env())
+			}
+		)
+
+		cy.visit('/')
+		cy.get('h1').first().invoke('text').should('equal', Cypress.env('envLabel'))
 	})
 })
