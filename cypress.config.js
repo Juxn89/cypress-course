06c309552052cfa9b8b3c6f691a4f5c9ccd24300
Juan Gómez <gomezj01@stepstone.com>
2024-04-22 15:33:56 -0600

    Allure report

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 const webpack = require('@cypress/webpack-preprocessor')
 const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+const allureWritter = require('@shelex/cypress-allure-plugin')
 
 const values = {}
 
@@ -9,7 +10,8 @@ async function setupNodeEvents(on, config) {
 	// implement node event listeners here
 	config.env.variable_env= process.env.NODE_ENV ?? 'NO ENVIROMENT VARIABLES SETTED'
 
-	addMatchImageSnapshotPlugin(on, config),
+	addMatchImageSnapshotPlugin(on, config)
+
 	on("task", {
 		saveValue(value) { 
 			const key = Object.keys(value)[0]
@@ -47,6 +49,8 @@ async function setupNodeEvents(on, config) {
 			},
 		})
 	)
+
+	allureWritter(on, config)
 
 	return config
 }
